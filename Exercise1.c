3a84b2e6de50a08bf84bff3f0640ba686de4c79b
TheRealDeath <97710284+TheRealDeath@users.noreply.github.com>
2023-09-06 13:37:01 -0500

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -3,17 +3,17 @@
 int main()
 {
     char* name = (char*)malloc(sizeof(char) * 100);
-    printf("enter your name:");
+    printf("enter your name: ");
     scanf("%[^\n]%*c", name);
-    printf("Hello %s\n",name);
     int age;
     printf("Enter age: ");
     scanf("%d",&age);
-    printf("Your age is %d\n",age);
-    scanf("\n");
+    getchar();
     char* phone = (char*)malloc(sizeof(char)*12);
     printf("Enter a phone number:");
     scanf("%[^\n]%*c", phone);
-    printf("\nYour phone number is %s",phone);
+    printf("Your name is %s\n",name);
+    printf("Your age is %d\n",age);
+    printf("Your phone number is %s",phone);
     return 0;
 }
